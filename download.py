f3335407e8e78aeb895285d95292abc459be16a4
knightron0 <sarthak.robo@gmail.com>
2020-01-12 17:44:03 +0530

    adding progress

---
+++ download.py
@@ -0,0 +1,7 @@
+import os, requests
+from bs4 import BeautifulSoup
+ 
+def main(query):
+    url = "https://www.youtube.com/results?search_query=" + query
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
