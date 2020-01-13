924e81743bd8fddf483dc2d47d0d469bfad28526
knightron0 <sarthak.robo@gmail.com>
2020-01-13 18:18:50 +0530

    Works

--- download.py
+++ download.py
@@ -1,8 +1,31 @@
 import os, requests
 from bs4 import BeautifulSoup
- 
-def main(query):
+import youtube_dl
+
+download_options = {
+	'format': 'bestaudio/best',
+	'outtmpl': '%(title)s.%(ext)s',
+	'nocheckcertificate': True,
+	'postprocessors': [{
+		'key': 'FFmpegExtractAudio',
+		'preferredcodec': 'mp3',
+		'preferredquality': '192',
+	}],
+}
+
+def geturl(query):
     url = "https://www.youtube.com/results?search_query=" + query
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    
+    divs = soup.select("div.yt-lockup-content h3 a")
+    if len(divs) == 0:
+        print("Couldn't find song.")
+        return False
+    finurl = "https://www.youtube.com" + divs[0]["href"]
+    return finurl
+
+def main(query):
+    with youtube_dl.YoutubeDL(download_options) as dl:
+        urls = geturl(query)
+        if urls != False:
+            dl.download([urls])
--- upload.py
+++ upload.py
@@ -1,6 +1,32 @@
 from gmusicapi import Mobileclient, Musicmanager
 
 mm = Musicmanager()
-mm.perform_oauth()
+api = Mobileclient()
+# api.perform_oauth()
+api.oauth_login(Mobileclient.FROM_MAC_ADDRESS)
+# mm.perform_oauth()
 mm.login()
-mm.upload("test.mp3")
+plid = ""
+def createplist(name):
+    plid = api.create_playlist(name)
+
+def upload(path):
+    mm.upload(path)
+    addplaylist(path)
+
+sids = []
+
+
+def addplaylist(title):
+    lst = mm.get_uploaded_songs()
+    sid = ""
+    for i in lst:
+        if(i["title"]==title):
+            sid = i["id"]
+            break
+    sids.append(sid)
+
+def appendtoplaylist():
+    print("F")
+    print(sids)
+    api.add_songs_to_playlist(plid, sids)
